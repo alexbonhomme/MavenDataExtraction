615026cd943b937308322e5534e7240ff4388c85
blckshrk <alexand.bonhom@gmail.com>
2013-12-11 20:32:04 +0100

    Configuring of the scraping from the root or from the 'com' package root

--- src/main/python/fr/lille1/maven_data_extraction/main.py
+++ src/main/python/fr/lille1/maven_data_extraction/main.py
@@ -3,32 +3,37 @@ Last edit on 11 dec. 2013
 
 @author: Alexandre Bonhomme
 '''
-from fr.lille1.maven_data_extraction.scrapers.maven.maven_scraper import MavenScrape
+from fr.lille1.maven_data_extraction.scrapers.maven.maven_scraper import \
+    MavenScrape
 import logging as log
 import os
 import sys
 
 class Main(object):
 
-
     def __init__(self):
-        self.scraper = MavenScrape('en')
-
+        self.scraper = MavenScrape()
 
     '''
-    Run scraping and fills the database
+    Run scraping and .pom files
     '''
     def run(self):
-        self.scraper.setConfig('man', 'jeans', 'Jean', 'Bottom')
+        # We scraped all the artefacts (from the root of MCR)
+        # self.scraper.setConfig(47)
+
+        # Only the children of 'com' package
+        self.scraper.setConfig(3059406)
+
+        # Go!
         itemList = self.scraper.run();
 
-        log.info('\n-- Scraping DONE ! --')
+        log.info('-- Scraping DONE ! --')
 
 '''
 Main
 '''
 if __name__ == '__main__':
-    log.basicConfig(level = log.DEBUG)
+    log.basicConfig(level = log.INFO)
 
     '''
     if len(sys.argv) < 2:
