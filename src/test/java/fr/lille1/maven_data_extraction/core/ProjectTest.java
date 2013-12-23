67808e39bf9034b1891faf8aa9ae03953bd4a5df
blckshrk <alexand.bonhom@gmail.com>
2013-12-23 16:05:26 +0100

    Testing of the Project object, essentially methods to managers Versions

---
+++ src/test/java/fr/lille1/maven_data_extraction/core/ProjectTest.java
@@ -0,0 +1,42 @@
+package fr.lille1.maven_data_extraction.core;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProjectTest {
+
+	private Project project;
+
+	@Before
+	public void setUp() throws Exception {
+		project = new Project("com.googlecode.jmockit", "guava");
+		project.addVersion(new Version("0.0.1", new File("")));
+		project.addVersion(new Version("0.1.1", new File("")));
+	}
+
+	@Test
+	public void testAddVersion() {
+		Version newVersion = new Version("0.2", new File(""));
+		project.addVersion(newVersion);
+		assertEquals(3, project.getVersionsSize());
+		assertEquals(newVersion, project.getVersion("0.2"));
+	}
+
+	@Test
+	public void testRemoveVersion() {
+		project.removeVersion("0.0.1");
+		assertEquals(1, project.getVersionsSize());
+		assertEquals(null, project.getVersion("0.0.1"));
+	}
+
+	@Test
+	public void testEqualsObject() {
+		Project newProject = new Project("com.googlecode.jmockit", "guava");
+		newProject.equals(project);
+	}
+
+}
