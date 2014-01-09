31a8da65a3d61313f42c30a19c5715468756b119
Clem-me <dufour.clement@gmail.com>
2014-01-09 03:29:56 +0100

    Add constructor for DataExtractionImpl

--- src/main/java/fr/lille1/maven_data_extraction/DataExtractionImpl.java
+++ src/main/java/fr/lille1/maven_data_extraction/DataExtractionImpl.java
@@ -1,6 +1,7 @@
 package fr.lille1.maven_data_extraction;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,31 +11,95 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import fr.lille1.maven_data_extraction.core.Project;
+import fr.lille1.maven_data_extraction.core.Version;
 
 public class DataExtractionImpl implements DataExtraction {
 
 	public File folder;
-	public Project projet;
-
-	public DataExtractionImpl(File folder, Project projet) {
+	public Project project;
+	public Version version;
+	
+	public DataExtractionImpl(Project project, Version version, File folder){
 		this.folder = folder;
-		this.projet = projet;
+		this.project = project;
+		this.version = version;
 	}
 
-
-	/* (non-Javadoc)
-	 * @see fr.lille1.maven_data_extraction.DataExtraction#findPom(java.io.File)
-	 */
-	public List<File> findPom(File folder) {
-		List<File> listFile =  new ArrayList<File>();
+	public DataExtractionImpl(File pom, File folder) throws JDOMException, IOException {
+		this.folder = folder;
+		project = getProject(pom);
+		version = getVersion(pom);
 		
+		System.out.println(project.getGroupId());
+		System.out.println(project.getArtifactId());
+		System.out.println(version.getVersionNumber());
+	}
+
+	private Project getProject(File pom){
+		String groupId;
+		String artifactId;
+		SAXBuilder builder = new SAXBuilder();
+
+		try {
+			Document document = (Document) builder.build(pom);
+			Element rootNode = document.getRootElement();			
+			
+			groupId = rootNode.getChildText("groupId", rootNode.getNamespace());
+			artifactId = rootNode.getChildText("artifactId", rootNode.getNamespace());
+			
+			if (groupId != null && artifactId != null){
+				return new Project(groupId, artifactId);
+			}
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	private Version getVersion(File pom) {
+		String versionNumber;
+		SAXBuilder builder = new SAXBuilder();
+
+		try {
+			Document document = (Document) builder.build(pom);
+			Element rootNode = document.getRootElement();
+			
+			versionNumber = rootNode.getChildText("version", rootNode.getNamespace());
+			
+			if (versionNumber != null){
+				return new Version(versionNumber, pom);
+			}
+			
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	public List<File> findPom(File folder) {
+		List<File> listFile = new ArrayList<File>();
+
 		File[] files = folder.listFiles();
-		for (File file : files){
-			if (file.isDirectory()){
+		for (File file : files) {
+			if (file.isDirectory()) {
 				listFile.addAll(findPom(file));
 			} else {
 				listFile.add(file);
@@ -43,33 +108,12 @@ public class DataExtractionImpl implements DataExtraction {
 		return listFile;
 	}
 
-	
-	/* (non-Javadoc)
-	 * @see fr.lille1.maven_data_extraction.DataExtraction#getDependent(java.io.File)
-	 */
-	public Project getDependent(File file) throws XPathExpressionException{
-		Project projet = null;
-		boolean hasParent = false;
+	public Project getDependent(File xmlFile) throws XPathExpressionException{
 		
-		XPathFactory factory = XPathFactory.newInstance();
-		XPath xpath = factory.newXPath();
-		XPathExpression expr = xpath.compile("//project/*");
-		
-		
-		Object result = expr.evaluate(file, XPathConstants.NODESET);
-		NodeList nodes = (NodeList) result;
-		for (int i = 0; i < nodes.getLength(); i++) {
-			Node node = nodes.item( i );
-			String nodeName = node.getNodeName();
-			String nodeValue = node.getChildNodes().item( 0 ).getNodeValue();
-			
-			if( nodeName.equals( "parent" ) ) {
-				hasParent = true;
-    }
-		}
-		
-		
-		return projet;
-	}
 
+
+		
+		
+		return null;
+	}
 }
--- src/main/java/fr/lille1/maven_data_extraction/Main.java
+++ src/main/java/fr/lille1/maven_data_extraction/Main.java
@@ -1,26 +1,22 @@
 package fr.lille1.maven_data_extraction;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import org.jdom2.JDOMException;
 
 import fr.lille1.maven_data_extraction.core.Project;
 import fr.lille1.maven_data_extraction.core.Version;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JDOMException, IOException {
 		
-		Project projet = new Project("com.googlecode.jmockit", "guava");
-		projet.addVersion(new Version("", new File(
-				"~/workspace/IDL/MavenDataExtraction/testGeneral/source")));
-		File folder = new File("");
+		File pom = new File("/home/klem/Documents/maven_central/download/asia/ivity/android/drag-sort-listview/1.0/drag-sort-listview-1.0.pom");
 		
-		DataExtraction dex = new DataExtractionImpl(folder, projet);
+		DataExtraction dex = new DataExtractionImpl(pom, new File("/"));
 		
-		List<File> listFile = dex.findPom(folder);
-		for (File file : listFile) {
-			System.out.println(file.toString());
-		}
 	}
 
 }
