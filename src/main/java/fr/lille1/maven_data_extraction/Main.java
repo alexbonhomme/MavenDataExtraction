5ac47bc422b072fe7d2d6d8bc775eeda91146c0a
blckshrk <alexand.bonhom@gmail.com>
2014-01-13 20:43:22 +0100

    Implmentation of an interractive console, to get stats from any artifact (e.g. stats org.apache.mypackage)

--- src/main/java/fr/lille1/maven_data_extraction/Main.java
+++ src/main/java/fr/lille1/maven_data_extraction/Main.java
@@ -1,44 +1,80 @@
 package fr.lille1.maven_data_extraction;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.io.IOException;
+import java.util.Scanner;
 
-import fr.lille1.maven_data_extraction.core.Project;
-import fr.lille1.maven_data_extraction.core.Version;
+import org.apache.log4j.Logger;
+
+import com.google.gag.annotation.disclaimer.AhaMoment;
+import com.google.gag.enumeration.Where;
+
 import fr.lille1.maven_data_extraction.core.extraction.DataExtraction;
 import fr.lille1.maven_data_extraction.core.extraction.DataExtractionImpl;
+import fr.lille1.maven_data_extraction.core.graph.MavenLabeledEdge;
+import fr.lille1.maven_data_extraction.core.graph.MavenMultigraph;
+import fr.lille1.maven_data_extraction.core.graph.MavenMultigraphFactory;
+import fr.lille1.maven_data_extraction.core.graph.MavenMultigraphLabeled;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		File pom = new File("/home/klem/Documents/maven_central/download/asia/ivity/android/drag-sort-listview/1.0/drag-sort-listview-1.0.pom");
-		File pom2 = new File("/home/klem/Documents/maven_central/download/com/legsem");
-		
-		
-		DataExtraction dex = new DataExtractionImpl(pom2);
-		HashMap<String, Project> map = dex.getAllProject();
-		
-		for (Map.Entry<String, Project> res : map.entrySet()) {
-			
-			System.out.println("====Project==== " + res.getKey());
-			
-			Iterator iterator = res.getValue().getVersionsIterator();
-			
-			while(iterator.hasNext()) {
-				Version resVersion =  (Version) iterator.next();
-				System.out.println("===Version=== " + resVersion.getVersionNumber());
-				
-				for (Project projectDep : resVersion.getDependencies()){
-					System.out.println("==projectDep==" + projectDep.getGroupId() + "." + projectDep.getArtifactId());
-					System.out.println("=versionDep=");
-				}
-			}
-			
+	private final static Logger log = Logger.getLogger(Main.class);
+
+	@AhaMoment(Where.TOILET)
+	public static void main(String[] args) throws IOException {
+		if (args.length < 1) {
+			throw new IllegalArgumentException("Usage : Main /path/to/pom/file");
 		}
-		
+
+		File root = new File(args[0]);
+		log.trace(root.getCanonicalPath());
+
+		/*
+		 * Graph creation
+		 */
+		MavenMultigraphFactory factory = new MavenMultigraphFactory(
+				MavenMultigraphLabeled.class);
+		DataExtraction extractor = new DataExtractionImpl(root);
+
+		log.info("Starting graph creation...");
+		MavenMultigraph<MavenLabeledEdge> graph = (MavenMultigraph<MavenLabeledEdge>) factory
+				.build(extractor);
+
+		/*
+		 * Console running
+		 */
+		System.out.println("###############################");
+		System.out.println("#");
+		System.out.println("# Maven Data Extraction");
+		System.out.println("#");
+		System.out.println("###############################");
+
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.print("> ");
+			String cmd = scanner.nextLine();
+
+			String[] cmdTab = cmd.split(" ");
+			switch (cmdTab[0]) {
+			case "stats":
+				if (cmdTab.length < 2) {
+					System.out.println("Usage : stats groupIp.artifactId");
+					continue;
+				}
+
+				System.out.println("Statistics for " + cmdTab[1] + " :");
+				break;
+
+			case "exit":
+				scanner.close();
+				System.out.println("Bye!");
+				return;
+
+			default:
+				System.out.println("Command not found.");
+				break;
+			}
+		}
 	}
 
 }
