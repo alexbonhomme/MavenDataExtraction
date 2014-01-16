d881e6b4046ff50650dd5615586f052bce14a2fe
blckshrk <alexand.bonhom@gmail.com>
2014-01-16 16:03:31 +0100

    Creation of an interface for the metrics in the console and an implementations using Jython

---
+++ src/main/java/fr/lille1/maven_data_extraction/console/MavenMetricsConsole.java
@@ -0,0 +1,37 @@
+package fr.lille1.maven_data_extraction.console;
+
+import java.util.List;
+
+import fr.lille1.maven_data_extraction.core.Project;
+import fr.lille1.maven_data_extraction.core.Version;
+
+/**
+ * 
+ * @author Alexandre Bonhomme
+ * 
+ */
+public interface MavenMetricsConsole {
+
+	void printGeneralStats();
+
+	void printStats(String groupId, String artifactId);
+
+	List<Project> dependenciesOf(Project p);
+
+	List<Project> dependenciesOf(String groupId, String artifactId);
+
+	List<Project> dependenciesOf(Project p, Version v);
+
+	List<Project> dependenciesOf(String groupId, String artifactId,
+			String versionNumber);
+
+	List<Project> usagesOf(Project p);
+
+	List<Project> usagesOf(String groupId, String artifactId);
+
+	List<Project> usagesOf(Project p, Version v);
+
+	List<Project> usagesOf(String groupId, String artifactId,
+			String versionNumber);
+
+}
---
+++ src/main/java/fr/lille1/maven_data_extraction/console/MavenMetricsConsoleJython.java
@@ -0,0 +1,84 @@
+package fr.lille1.maven_data_extraction.console;
+
+import java.util.List;
+
+import fr.lille1.maven_data_extraction.core.Project;
+import fr.lille1.maven_data_extraction.core.Version;
+import fr.lille1.maven_data_extraction.core.graph.MavenLabeledEdge;
+import fr.lille1.maven_data_extraction.core.graph.MavenMultigraph;
+import fr.lille1.maven_data_extraction.core.metrics.MavenMetrics;
+import fr.lille1.maven_data_extraction.core.metrics.MavenMetricsImpl;
+
+public class MavenMetricsConsoleJython implements MavenMetricsConsole {
+
+	private final MavenMultigraph<MavenLabeledEdge> graph;
+	private final MavenMetrics metrics;
+
+	public MavenMetricsConsoleJython(MavenMultigraph<MavenLabeledEdge> graph) {
+		this.graph = graph;
+		this.metrics = new MavenMetricsImpl(graph);
+	}
+
+	@Override
+	public void printGeneralStats() {
+		String out = new String();
+
+		out += "\tNumber of artifacts: " + graph.getAllVertices().size();
+
+		System.out.println(out);
+	}
+
+	@Override
+	public void printStats(String groupId, String artifactId) {
+		String out = new String();
+
+		out += "\tDependencies: " + dependenciesOf(groupId, artifactId).size();
+		out += "\n\tUsages: " + usagesOf(groupId, artifactId).size();
+
+		System.out.println(out);
+	}
+
+	@Override
+	public List<Project> dependenciesOf(Project p) {
+		return metrics.computeDependencies(p);
+	}
+
+	@Override
+	public List<Project> dependenciesOf(String groupId, String artifactId) {
+		return dependenciesOf(new Project(groupId, artifactId));
+	}
+
+	@Override
+	public List<Project> dependenciesOf(Project p, Version v) {
+		return metrics.computeDependencies(p, v);
+	}
+
+	@Override
+	public List<Project> dependenciesOf(String groupId, String artifactId,
+			String versionNumber) {
+		return dependenciesOf(new Project(groupId, artifactId), new Version(
+				versionNumber));
+	}
+
+	@Override
+	public List<Project> usagesOf(Project p) {
+		return metrics.computeAllUsages(p);
+	}
+
+	@Override
+	public List<Project> usagesOf(String groupId, String artifactId) {
+		return usagesOf(new Project(groupId, artifactId));
+	}
+
+	@Override
+	public List<Project> usagesOf(Project p, Version v) {
+		return metrics.computeUsages(p, v);
+	}
+
+	@Override
+	public List<Project> usagesOf(String groupId, String artifactId,
+			String versionNumber) {
+		return usagesOf(new Project(groupId, artifactId), new Version(
+				versionNumber));
+	}
+}
